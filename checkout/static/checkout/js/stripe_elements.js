b3800081fc5cc08f172ba2b06cdd555e0f934303
SohailMohammed1 <sohailm2233@gmail.com>
2023-11-24 00:29:21 +0000

    added working form with card info section

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,12 +6,7 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
-var elements = stripe.elements();
-var card = elements.create('card', {style: style});
-
+// Define the style variable before using it to create the card element
 var style = {
     base: {
         color: "#000",
@@ -28,6 +23,11 @@ var style = {
     }
 };
 
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+var card = elements.create('card', { style: style });
 card.mount('#card-element');
 
 // Handle realtime validation errors on the card element
@@ -46,4 +46,47 @@ card.addEventListener('change', function (event) {
     }
 });
 
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true });
+    $('#submit-button').attr('disabled', true);
+
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            // Inform the user if there was an error
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>
+            `;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false });
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                // Clear the form here
+                form.reset();
+                card.clear();
+
+                // Optionally, redirect the user or display a success message
+                // window.location.href = "/success-page"; // Redirect to a success page
+                // or
+                messages.success(request, "Your payment was successful!"); // Display a success message
+
+                // Re-enable the card and submit button
+                card.update({ 'disabled': false });
+                $('#submit-button').attr('disabled', false);
+            }
+        }
+    });
+});
 
--- checkout/views.py
+++ checkout/views.py
@@ -8,6 +8,9 @@ from bag.contexts import bag_contents
 import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -18,13 +21,24 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create (
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY, 
+    )
+
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing')
+
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form,
-        'stripe_public_key': 'pk_test_51OFh3HHAgoExX0K4wJEubBV50fyRhZoZAB7TOFeCG43sFHkcK0pRa2N1tF14IZ27m3A93MX7WDthKxPwvh1UVPD60069LClVqa',
-        'client_secret': 'test client secret', 
-    }
+    'order_form': order_form,
+    'stripe_public_key': stripe_public_key,  
+    'client_secret': intent.client_secret, 
+}
+
 
     return render(request, template, context)
