af9744f996b2206915f579819ca02a51e3b88561
SohailMohammed1 <sohailm2233@gmail.com>
2024-03-11 15:41:14 +0000

    third deployment attempt

--- eye_in_the_sky/settings.py
+++ eye_in_the_sky/settings.py
@@ -183,7 +183,7 @@ STATICFILES_DIRS= (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if 'USE_AWS' in os.eviron:
+if 'USE_AWS' in os.environ:
     # Bucket Configuration 
     AWS_STORAGE_BUCKET_NAME = 'sm1-eye-in-the-sky'
     AWS_S3_REGION_NAME = 'eu-west-2'
